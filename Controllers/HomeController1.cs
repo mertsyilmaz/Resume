f27fd3d1e4f59ac82c0330a7f49da70cd334b5d0
mertsyilmaz <36952537+mertsyilmaz@users.noreply.github.com>
2021-02-08 20:56:18 +0100

    aaaa

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResumeMvcCore.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
